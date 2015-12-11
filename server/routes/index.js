571e8818eabeca424a24b897692ad9a0fe685a3d
colophonemes <sam@Sam-Deeres-MBP.home>
2015-12-11 18:46:03 +0000

    Update title

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Thing' });
+  res.render('index', { title: 'CEA Donations' });
 });
 
 module.exports = router;
