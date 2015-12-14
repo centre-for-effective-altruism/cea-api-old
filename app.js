f5812fd2eaf3dddbafa1c277ad0b32eb2db0b950
colophonemes <sam@Sam-Deeres-MBP.home>
2015-12-14 17:25:35 +0000

    Tidy app setup

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var express = require('express');
 var path = require('path');
 
 
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -18,24 +17,18 @@ var api = require('./server/routes/api/index');
 var app = express();
 
 
-// enforce SSL
-
+// enforce SSL in production
 if(app.get('env') === 'production'){
   var enforce = require('express-sslify');
   app.use(enforce.HTTPS({ trustProtoHeader: true }))
 }
 
-// view engine setup
-app.set('views', path.join(__dirname, 'client', 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client','public')));
 
 app.use('/v1', api);
 app.use('/', routes);
